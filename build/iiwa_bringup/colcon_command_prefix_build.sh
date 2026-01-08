b7f6c82ad58effb37bbc9af047b5d1315685bd9c
Andrearusso01 <andrea.russo51@studenti.unina.it>
2026-01-08 15:56:31 +0000

    final

---
+++ build/iiwa_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/user/ros2_ws/install/iiwa_description/share/iiwa_description/package.sh"
---
+++ install/ros2_fra2mo/share/ros2_fra2mo/launch/warehouse.launch.py
@@ -0,0 +1,260 @@
+import os
+from launch import LaunchDescription
+from launch.actions import (
+    TimerAction, 
+    DeclareLaunchArgument, 
+    SetEnvironmentVariable, 
+    IncludeLaunchDescription, 
+    RegisterEventHandler
+)
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    # --- Environment: ros2_control plugin ---
+    set_plugin_path = SetEnvironmentVariable(
+        name='IGN_GAZEBO_SYSTEM_PLUGIN_PATH',
+        value=[
+            '/opt/ros/humble/lib:',
+            os.environ.get('IGN_GAZEBO_SYSTEM_PLUGIN_PATH', '')
+        ]
+    )
+
+    # --- Paths ---
+    world_file = os.path.join(
+        get_package_share_directory('ros2_fra2mo'),
+        'worlds', 'warehouse.sdf'
+    )
+
+    # Fra2mo description
+    fra2mo_xacro = os.path.join(
+        get_package_share_directory('ros2_fra2mo'),
+        'urdf', 'fra2mo.urdf.xacro'
+    )
+
+    fra2mo_desc = {
+        "robot_description": ParameterValue(
+            Command(['xacro ', fra2mo_xacro]),
+            value_type=str
+        )
+    }
+
+    # --- IIWA Arguments ---
+    iiwa_description_arg = DeclareLaunchArgument(
+        'iiwa_description_file',
+        default_value='iiwa.config.xacro'
+    )
+
+    config_file = LaunchConfiguration('iiwa_description_file')
+
+    # --- IIWA Robot description ---
+    xacro_path = PathJoinSubstitution([
+        FindPackageShare('iiwa_description'),
+        'config',
+        config_file
+    ])
+   
+    iiwa_pose_config = PathJoinSubstitution([
+        FindPackageShare('ros2_fra2mo'),
+        'config',
+        'iiwa.pose.yaml'
+    ])
+
+    iiwa_desc = {
+        'robot_description': ParameterValue(
+            Command([
+                'xacro ', xacro_path,
+                ' prefix:=iiwa_',
+                ' parent:=world',
+                ' use_sim:=true',
+                ' namespace:=iiwa',
+                ' command_interface:=velocity',
+                ' base_frame_file:=', iiwa_pose_config
+            ]),
+            value_type=str
+        )
+    }
+
+    # --- Robot state publishers ---
+    fra2mo_rsp = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        namespace='fra2mo',
+        parameters=[fra2mo_desc],
+        output='screen'
+    )
+
+    iiwa_rsp = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        namespace='iiwa',
+        parameters=[iiwa_desc],
+        remappings=[
+            ('/robot_description', 'robot_description')
+        ],
+        output='screen'
+    )
+
+    # -------------------------
+    # BRIDGES & TF
+    # -------------------------
+
+    # 1. Bridge Gazebo <-> ROS 2
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            # --- ROBOT FRA2MO (Rover) ---
+            '/cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist',
+            '/model/fra2mo/odometry@nav_msgs/msg/Odometry@ignition.msgs.Odometry',
+            '/model/fra2mo/tf@tf2_msgs/msg/TFMessage@ignition.msgs.Pose_V',
+            '/lidar@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan',
+
+            # --- SISTEMA ---
+            '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
+
+            # --- CAMERA ---
+            #'/camera/image_raw@sensor_msgs/msg/Image@ignition.msgs.Image',
+            #'/camera/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo',
+            
+            # --- CAMERA BRIDGE (CORRETTO) ---
+            '/iiwa_camera/image_raw@sensor_msgs/msg/Image[ignition.msgs.Image',
+            '/iiwa_camera/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+
+            # --- BRACCIO IIWA ---
+            # Lettura stato giunti (per RViz)
+            '/iiwa/joint_states@sensor_msgs/msg/JointState@ignition.msgs.Model',
+            '/tf_static@tf2_msgs/msg/TFMessage[ignition.msgs.Pose_V',
+
+            # Invio comandi movimento
+            '/model/iiwa/joint_trajectory@trajectory_msgs/msg/JointTrajectory]ignition.msgs.JointTrajectory'
+        ],
+        output='screen'
+    )
+    
+    # 2. Odom TF Publisher
+    odom_tf = Node(
+        package='ros2_fra2mo',
+        executable='dynamic_tf_publisher',
+        name='odom_tf',
+        parameters=[{"use_sim_time": True}],
+        output='screen'
+    )
+
+    # -------------------------
+    # Gazebo
+    # -------------------------
+    gz_args = DeclareLaunchArgument(
+        'gz_args',
+        default_value=world_file + ' -r',
+        description='Gazebo world'
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare('ros_gz_sim'),
+                'launch', 'gz_sim.launch.py'
+            ])
+        ),
+        launch_arguments={'gz_args': LaunchConfiguration('gz_args')}.items()
+    )
+
+    # -------------------------
+    # Spawn robots
+    # -------------------------
+    
+    # Spawn fra2mo
+    spawn_fra2mo = Node(
+        package='ros_gz_sim',
+        executable='create',
+	arguments=[
+            '-topic', '/fra2mo/robot_description',
+            '-name', 'fra2mo',
+            '-x', '7.2',
+            '-y', '-4.35',
+            '-z', '0.15'
+        ],
+        output='screen'
+    )
+    
+    # Spawn iiwa
+    spawn_iiwa = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-topic', '/iiwa/robot_description',
+            '-name', 'iiwa',
+            '-x', '8.0',
+            '-y', '-4.0',
+            '-z', '0.1'
+        ],
+        output='screen'
+    )
+
+    # --- Controllers (IIWA) ---
+    joint_state_broadcaster = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster'],
+        namespace='iiwa',
+        output='screen'
+    )
+
+    # Nota: arm_controller è commentato, uso velocity_controller
+    arm_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['iiwa_arm_controller'],
+        namespace='iiwa',
+       output='screen'
+    )
+
+    velocity_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['velocity_controller', '-c', '/iiwa/controller_manager'],
+        namespace='iiwa',
+        output='screen'
+    )
+
+    # Gestore eventi: avvia i controller DOPO che iiwa è stato spawnato
+    controllers_after_spawn = RegisterEventHandler(
+        OnProcessExit(
+            target_action=spawn_iiwa,
+            on_exit=[
+                joint_state_broadcaster,
+                # arm_controller,
+                velocity_controller
+            ]
+        )
+    )
+
+    # -------------------------
+    # Launch description
+    # -------------------------
+    return LaunchDescription([
+        SetEnvironmentVariable(
+            name="GZ_SIM_RESOURCE_PATH",
+            value=os.path.join(get_package_share_directory('ros2_fra2mo'), 'models') + ':' +
+                  os.path.join(get_package_share_directory('iiwa_description')) + ':' +
+                  os.environ.get('GZ_SIM_RESOURCE_PATH', '')
+        ),
+        iiwa_description_arg,
+        gz_args,
+        bridge,
+        odom_tf,
+        gazebo,
+        fra2mo_rsp,
+        spawn_fra2mo,
+        iiwa_rsp,
+        spawn_iiwa,
+        controllers_after_spawn
+    ])
